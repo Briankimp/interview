286f174f6833f30837437e44e50d3a362b6241f6
Briankimp <bkimathi973@gmail.com>
2025-07-03 23:34:35 +0300

    feat(theme): configure custom UI Theme

---
+++ app/theme.ts
@@ -0,0 +1,33 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#6F2C6F",
+
+      },
+      secondary: {
+        main: "#F3E6F3",
+      },
+      background: {
+        default: "#FFFFFF",
+      },
+      error: {
+        main: "#D55A5A",
+      },
+      warning: {
+        main: "#D55A5A",
+      },
+      
+      success: {
+        main: "#3AAB67",
+      },
+      text: {
+        primary: "#252428",
+        secondary: "#FFFFFF",
+      },
+    },
+});
+
+export default theme;
+
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import { config } from "process";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    };
+  
+    return config;
+  },
 };
 
 export default nextConfig;
